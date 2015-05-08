f06c0d79936af7df23791bf6be97d815cfa3c9b1
Britt Mathis <britt.mathis@gmail.com>
2015-05-07 20:37:01 -0400

    initial stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slackbot
+
+Slackbot is a simple chat bot which connects to the Real Time Messaging API
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+# to use consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# get long description from README
+with open(path.join(here, "README.md"), encoding="utf-8") as desc:
+    long_description = desc.read()
+
+setup(
+    name="slackbot",
+    version="0.0.1",
+    description="a simple slack bot",
+    long_description=long_description,
+    url="https://github.com/bmuk/slackbot",
+    author="Britt Mathis",
+    author_email="britt.mathis@gmail.com",
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Communications :: Chat",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords="slack bot chat",
+    packages=find_packages(),
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": ["slackbot=slackbot:main"]
+    }
+)
